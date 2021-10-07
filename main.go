091531d7f243dde428bc941258bcede362bae394
MathOli <matheus.oliveira2100@hotmail.com>
2021-10-07 15:48:15 -0300

    Loop and Delay

--- main.go
+++ main.go
@@ -2,33 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
+)
+
+const (
+	monitoramentos = 5
+	delay          = 3
 )
 
 func main() {
-	var (
-		nome   string  = "Matheus"
-		versao float32 = 1.1
-		opcao  int
-	)
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Esse programa esta na versão", versao)
 
-	menu()
+	for {
+		var (
+			nome   string  = "Matheus"
+			versao float32 = 1.3
+			opcao  int
+		)
+		fmt.Println("Olá, sr.", nome)
+		fmt.Println("Esse programa esta na versão", versao)
 
-	opcao = leOpcao()
+		menu()
 
-	switch opcao {
-	case 1:
-		fmt.Println("Monitoramento")
-	case 2:
-		fmt.Println("Logs")
-	case 0:
-		fmt.Println("Esta deixando o Sistema")
-		os.Exit(0)
-	default:
-		fmt.Println("Essa opção não existe")
+		opcao = leOpcao()
+
+		switch opcao {
+		case 1:
+			monitoramento()
+		case 2:
+			fmt.Println("Logs")
+		case 0:
+			fmt.Println("Esta deixando o Sistema")
+			os.Exit(0)
+		default:
+			fmt.Println("Essa opção não existe")
+			os.Exit(-1)
+		}
 	}
+
 }
 
 func menu() {
@@ -40,10 +52,33 @@ func menu() {
 func leOpcao() int {
 	var opcao int
 	fmt.Scan(&opcao)
+	fmt.Println("")
 
 	return opcao
 }
 
 func monitoramento() {
+	fmt.Println("Monitoramento...")
+	sites := []string{"https://www.youtube.com", "https://random-status-code.herokuapp.com"}
 
+	for i := 0; i < monitoramentos; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Foi carregado com sucesso")
+	} else {
+		fmt.Println("Possue algum error: ", resp.StatusCode)
+	}
 }
